269ea1c9c777ed7399c7981869feda531cefee59
lokesh-dadhich <165267716+lokesh-dadhich@users.noreply.github.com>
2025-09-16 12:25:52 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -1,46 +1,46 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-SRVPORT=4499
+# Default port (can override with ENV SRVPORT)
+SRVPORT=${SRVPORT:-4499}
 RSPFILE=response
 
 rm -f $RSPFILE
 mkfifo $RSPFILE
 
 get_api() {
-	read line
-	echo $line
+    read line
+    echo $line
 }
 
 handleRequest() {
-    # 1) Process the request
-	get_api
-	mod=`fortune`
+    get_api
+    mod=$(fortune)
 
-cat <<EOF > $RSPFILE
+    cat <<EOF > $RSPFILE
 HTTP/1.1 200
 
 
-<pre>`cowsay $mod`</pre>
+<pre>$(cowsay "$mod")</pre>
 EOF
 }
 
 prerequisites() {
-	command -v cowsay >/dev/null 2>&1 &&
-	command -v fortune >/dev/null 2>&1 || 
-		{ 
-			echo "Install prerequisites."
-			exit 1
-		}
+    command -v cowsay >/dev/null 2>&1 &&
+    command -v fortune >/dev/null 2>&1 ||
+    {
+        echo "Install prerequisites."
+        exit 1
+    }
 }
 
 main() {
-	prerequisites
-	echo "Wisdom served on port=$SRVPORT..."
+    prerequisites
+    echo "Wisdom served on port=$SRVPORT..."
 
-	while [ 1 ]; do
-		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
-		sleep 0.01
-	done
+    while true; do
+        cat $RSPFILE | nc -lN $SRVPORT | handleRequest
+        sleep 0.01
+    done
 }
 
 main
